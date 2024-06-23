ff0d70d79901d056c6c2695c4c09b5ec699001d0
Ishika-45 <bansalishika58@gmail.com>
2024-06-23 17:57:43 +0530

    Features added

---
+++ script.js
@@ -0,0 +1,21 @@
+// mixitup
+// var $container = $('.container'),
+// $controls = $('.controls');
+
+// // $('.container').mixItUp({
+// $($container).mixItUp({
+// controls: {
+//   enable: false
+// }
+// });
+
+// $controls.on('click', '.filter', function(){
+// var $btn = $(this),
+//     filter = $btn.attr('data-filter');
+
+// $btn.addClass('active').siblings().removeClass('active');
+
+// $container.mixItUp('filter', 'none', function(){
+//   $container.mixItUp('filter', filter);
+// });
+// });
